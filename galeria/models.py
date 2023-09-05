c0ef05cf453a5a785431d0d6d166bd425dab1e5d
vinicius481 <vasconcelosvinicius383@gmail.com>
2023-09-05 19:55:14 -0300

    vai

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 class  Fotografia(models.Model):
+
+    Opcoes_Categoria= [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
+        ("SATÉLITE NATURAL", "satélite natural"),
+
+    ] 
+
     nome = models.CharField(max_length= 100, null = False, blank = False) # black e para não permite que exitas "" <== strings vazis
     legenda = models.CharField(max_length= 150, null = False, blank = False)
+    categoria = models.CharField(max_length=100, choices= Opcoes_Categoria, default='')
     descricao = models.TextField(null = False, blank = False)
     foto = models.CharField(max_length= 100, null = False, blank = False)
 
