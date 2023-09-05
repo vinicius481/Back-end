dfa7671d52be1eaa52396333fa4d3f0c14af2343
vinicius481 <126528541+vinicius481@users.noreply.github.com>
2023-09-05 18:56:33 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+pull
 """
 
 from pathlib import Path, os
